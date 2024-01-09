c963ce8513e47a557ba9556ccc752e3fec06e9e4
breixs <paulbrei25@gmail.com>
2024-01-09 21:07:14 +0200

    First Working Version

--- proiect/regine.cpp
+++ proiect/regine.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <string>
+#include <vector>
 
 using namespace std;
 
@@ -11,15 +11,30 @@ void creareTabla(int t[8][8])
            t[i][j]=0;
 }
 
-void afisareTabla(int t[8][8])
+void afisareTabla(vector<vector<int>>& solutii, int t[8][8])
 {
-    int i, j;
-    for(i=0;i<8;i++)
+    int contor=0;
+    if(solutii.empty())
     {
-        for(j=0;j<8;j++)
-            cout<<t[i][j];
-        cout<<endl;
+        cout<<"nu exista solutii"<<endl;
     }
+    else{
+        for(auto& s: solutii)
+        {
+            for(int i=0;i<8;i++)
+            {
+                for(int j=0;j<8;j++)
+                {
+                    cout<<s[i*8+j];
+                }
+                cout<<endl;
+            }
+            cout<<endl;
+            contor++;
+        }
+    }
+
+    cout<<"Numarul de solutii gasite: "<<contor;
 }
 
 bool esteValid(int t[8][8], int rand, int col)
@@ -27,50 +42,57 @@ bool esteValid(int t[8][8], int rand, int col)
     int i, j;
     for(i=0;i<col;i++)
     {
-        if(t[rand][i])
+        if(t[rand][i]==1)
             return false;
     }
     for(i=rand, j=col;i>=0&&j>=0;i--,j--)
     {
-        if(t[rand][col])
+        if(t[i][j]==1)
         return false;
     }
     for(i=rand, j=col; j>=0&&i<8;i++,j--)
     {
-        if(t[i][j])
+        if(t[i][j]==1)
         return false;
     }
     return true;
 }
 
-bool rezolvare(int t[8][8], int col) 
+void rezolvare(vector<vector<int>>& solutii, int t[8][8], int col) 
 { 
-    if (col >= 8) 
-        return true; 
-    for (int i = 0; i < 8; i++) { 
-        if (esteValid(t, i, col)) { 
-            t[i][col] = 1; 
-            if (rezolvare(t, col + 1)) 
-                return true; 
-            t[i][col] = 0; 
-        } 
-    } 
-    return false; 
+    int i, j;
+    if(col>=8)//daca s-au parcurs toate coloanele
+    {
+        vector<int> sol;
+        for(i=0;i<8;i++)
+        {
+            for(j=0;j<8;j++)
+            {
+                sol.push_back(t[i][j]);
+            }
+        }
+        solutii.push_back(sol);
+        return;
+    }
+    for(i=0;i<8;i++)
+    {
+        if(esteValid(t,i,col))
+        {
+            t[i][col]=1;
+            rezolvare(solutii, t, col+1);
+            t[i][col]=0;
+        }
+    }
 } 
-
 
 int main()
 {
-    int t[8][8];
+    int t[8][8], contor=0;
+    vector<vector<int>> solutii;
     creareTabla(t);
 
-    if (rezolvare(t, 0) == false) { 
-        printf("Solution does not exist"); 
-        return 0; 
-    } 
-
-    afisareTabla(t);
-    return true;
+    rezolvare(solutii, t, 0);
+    afisareTabla(solutii, t);
 
     return 0;
 }
