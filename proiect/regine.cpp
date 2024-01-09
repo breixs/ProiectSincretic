42f16b3b3a72b8104cd5a2aea54b3ba0cb89a7bb
breixs <paulbrei25@gmail.com>
2024-01-09 19:03:26 +0200

    Created Matrix

--- proiect/regine.cpp
+++ proiect/regine.cpp
@@ -3,7 +3,29 @@
 
 using namespace std;
 
+void creareTabala(int t[8][8])
+{
+    int i, j;
+     for(i=0;i<8;i++)
+        for(j=0;j<8;j++)
+           t[i][j]=0;
+}
+
+void afisareTabla(int t[8][8])
+{
+    int i, j;
+    for(i=0;i<8;i++)
+    {
+        for(j=0;j<8;j++)
+            cout<<t[i][j];
+        cout<<endl;
+    }
+}
+
 int main()
 {
+    int t[8][8];
+    creareTabala(t);
+    afisareTabla(t);
     return 0;
 }
